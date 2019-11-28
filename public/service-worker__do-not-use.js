247248972ae721c82f29b8520a1eb959fa92e04c
Fabian Mebus <hello@fabianmebus.com>
2019-11-28 18:06:20 +0100

    added different versions of the same service worker

---
+++ public/service-worker__do-not-use.js
@@ -0,0 +1,22 @@
+/* CAUTION! Do not use this code, that was just my starting point, there are a lot of problems here */
+
+const CACHE_NAME = 'offline';
+const OFFLINE_PAGE_URL = '/offline.html';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll([OFFLINE_PAGE_URL]);
+  })());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith((async () => {
+    try {
+      return await fetch(event.request);
+    } catch (error) {
+      const cache = await caches.open(CACHE_NAME);
+      return await cache.match(OFFLINE_PAGE_URL);
+    }
+  })());
+});
